b1dec3319289e256dfbbddd410e23bb7a3118e6d
TilakSN <tilakchinnu96@gmail.com>
2017-11-09 23:09:58 +0530

    Modifications done

--- data_set_generator.py
+++ data_set_generator.py
@@ -13,6 +13,8 @@ parser.add_argument('-n', '--max', help="the maximum marks for a question", type
 parser.add_argument('file', help="the output file name without extension. Saves student data to <file>_student.csv and question details to <file>_questions.csv")
 args = parser.parse_args()
 
+args.bloom = min(args.bloom, args.M)
+
 percent = np.random.rand(args.N) * 0.8 + 0.2
 
 per_question = np.repeat(percent[:, np.newaxis], args.M, 1)
@@ -25,7 +27,8 @@ filtered_data = np.clip(random_data, 0, 1)
 
 max_marks = np.random.randint(args.min, args.max + 1, args.M)
 
-bloom_level = np.random.randint(1, args.bloom + 1, args.M)
+bloom_level = np.hstack((np.random.randint(1, args.bloom + 1, args.M - args.bloom), np.arange(1, args.bloom + 1)))
+np.random.shuffle(bloom_level)
 
 data = np.around(filtered_data * max_marks * 2) / 2.0
 
@@ -39,14 +42,16 @@ for i, j in enumerate(question_data):
     m[j[1] - 1] += j[0]
 percent = np.divide(a, m, out=np.zeros_like(a), where=m != 0)
 
-target = np.argmax(percent, axis=1)
+target = np.argmax(percent, axis=1) + 1
 
 final_data = np.hstack((data, target.reshape((target.shape[0], 1))))
 
 df = pd.DataFrame(final_data)
 df.columns = [str(i + 1) for i in range(question_data.shape[0])] + ["Target"]
+df.index += 1
 df["Target"] = df["Target"].astype('int32')
-df.to_csv(args.file + "_student.csv", index=False)
+df.to_csv(args.file + "_student.csv", index_label="Roll No")
 df = pd.DataFrame(question_data)
-df.columns = ["MaxMarks", "BloomLevel"]
-df.to_csv(args.file + "_questions.csv", index=False)
+df.columns = ["Max Marks", "Bloom Level"]
+df.index += 1
+df.to_csv(args.file + "_questions.csv", index_label="Q#")
